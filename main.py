d1ac44f64af0fe2825035778e9bc732e0d9a1863
Lucas Laredo <luckrocaabh@gmail.com>
2024-10-19 17:45:12 -0300

    Model input handler *full* implementation

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import logging
+import pprint
+import sys
+
 from src.core.factory.factory import Factory
+
+pp = pprint.PrettyPrinter(indent=10)
 
 PATH: str = r"C:\Users\luckr\OneDrive\Área de Trabalho\hackaton-po\docs\cenario_1.xlsx"
 
@@ -10,9 +16,20 @@ def create_factory() -> Factory:
 def main():
     factory = create_factory()
     input_importer = factory.get_model_import_repository()
-    table = input_importer.handle_input(PATH)
-    return table
+    combination_dict, disponibility_patients, disponibility_professionals = (
+        input_importer.handle_input(PATH)
+    )
+    pp.pprint(disponibility_patients)
+    return combination_dict, disponibility_patients, disponibility_professionals
 
 
 if __name__ == "__main__":
+    # logger setup
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[logging.FileHandler("debug.log"), logging.StreamHandler(sys.stdout)],
+    )
+
+    # main function
     main()
--- src/core/config.py
+++ src/core/config.py
@@ -10,6 +10,25 @@ class Config:
         "ASA - SANTA MÔNICA",
         "ASA - PRIMAVERA",
         "ASA - SÃO JOSÉ",
+        "virtual_epsi",
+    ]
+
+    AGE_RANGE: list[str] = ["infantil", "adolescente", "adulto"]
+
+    AVAILABLE_HOURS: str = [
+        "hr_8",
+        "hr_9",
+        "hr_10",
+        "hr_11",
+        "hr_12",
+        "hr_13",
+        "hr_14",
+        "hr_15",
+        "hr_16",
+        "hr_17",
+        "hr_18",
+        "hr_19",
+        "hr_20",
     ]
 
 
